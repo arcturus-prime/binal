85fa104f883fd6a5ad45c248e9fda68b057ddeab
Arcturus Prime <seanans135@gmail.com>
2025-10-27 23:25:51 -0500

    oops

--- src/ir.rs
+++ src/ir.rs
@@ -1,19 +1,37 @@
-use std::fmt::Write;
+use std::{fmt::{Debug, Write}, ops::BitAnd};
 
 use num_enum::TryFromPrimitive;
 use num_traits::PrimInt;
 
+#[derive(Default)]
 pub struct ProgramWriter {
     code: Vec<u8>,
 }
 
 impl ProgramWriter {
     pub fn emit_instruction(&mut self, instruction: Instruction) {}
-    pub fn emit_constant<T: PrimInt>(&mut self, mut number: T) {}
-}
+    pub fn emit_constant<T: PrimInt + TryFrom<u8> + TryInto<u8>>(&mut self, mut number: T)
+    {
+        loop {
+            // SAFETY: number & 0x7f is always valid as u8 due to the bitmask
+            let v: u8 = unsafe { (number & 0x7f.try_into().unwrap_unchecked()).try_into().unwrap_unchecked() };
+            number = number >> 7;
+
+            self.code.push(v);
+
+            if (number == T::zero() && v & 0x40 == 0) || (number == T::max_value() && v & 0x40 != 0) {
+                break;
+            }
+        }
+    }
+
+    pub fn finish(self) -> Vec<u8> {
+        self.code
+    }
+} 
 
 #[repr(u8)]
-#[derive(TryFromPrimitive, Copy, Clone, Debug)]
+#[derive(TryFromPrimitive, Copy, Clone, Debug, PartialEq)]
 pub enum Instruction {
     Nop = 128,
 
@@ -59,6 +77,8 @@ pub enum Instruction {
     Copy, // value
     Pick, // number
 
+    Call, // destination
+
     Loop,
     If, // condition
     Else,
@@ -72,6 +92,7 @@ pub enum Instruction {
 impl Instruction {
     pub fn argument_count(self) -> usize {
         match self {
+            Instruction::Call => 1,
             Instruction::Nop => 0,
             Instruction::Boolean => 1,
             Instruction::Add => 2,
@@ -120,6 +141,7 @@ impl Instruction {
 
     pub fn product_count(self) -> usize {
         match self {
+            Instruction::Call => 0,
             Instruction::Nop => 0,
             Instruction::Boolean => 1,
             Instruction::Add => 1,
@@ -205,13 +227,37 @@ fn pop_safe(stack: &mut Vec<String>) -> Result<String, PrintError> {
     }
 }
 
+fn parse_varint<T: PrimInt + TryFrom<u8>>(data: &[u8]) -> T
+where
+    <T as TryFrom<u8>>::Error: Debug,
+{
+    let mut result = T::zero();
+    let mut last= 0;
+    let mut shift = 0;
+
+    for v in data {
+        let converted: T = (*v).try_into().unwrap();
+
+        result = result | (converted << shift);
+        shift += 7;
+
+        last = *v;
+    }
+
+    if data.len() < size_of::<T>() || last & 0x40 != 0 {
+        result = result | (T::max_value() << shift);
+    }
+
+    result
+}
+
 pub fn print_instructions(code: &[u8]) -> Result<(), PrintError> {
     let mut stack = vec![];
-    let mut const_stack = BitStack::new();
+    let mut const_stack = vec![];
 
     for x in code {
         if *x < 128 {
-            const_stack.push::<7, u8>(*x);
+            const_stack.push(*x);
             continue;
         }
 
@@ -220,6 +266,7 @@ pub fn print_instructions(code: &[u8]) -> Result<(), PrintError> {
         };
 
         let string = match x {
+            Instruction::Call => todo!(),
             Instruction::Nop => String::new(),
             Instruction::Add => todo!(),
             Instruction::Sub => todo!(),
@@ -251,19 +298,31 @@ pub fn print_instructions(code: &[u8]) -> Result<(), PrintError> {
             Instruction::BranchIndex => todo!(),
             Instruction::Return => todo!(),
             Instruction::Boolean => todo!(),
-            Instruction::U64 => const_stack.pop::<64, u64>().to_string(),
-            Instruction::U32 => const_stack.pop::<32, u32>().to_string(),
-            Instruction::U16 => const_stack.pop::<16, u16>().to_string(),
-            Instruction::U8 => const_stack.pop::<8, u8>().to_string(),
-            Instruction::I64 => const_stack.pop::<64, i64>().to_string(),
-            Instruction::I32 => const_stack.pop::<32, i32>().to_string(),
-            Instruction::I16 => const_stack.pop::<16, i16>().to_string(),
-            Instruction::I8 => const_stack.pop::<8, i8>().to_string(),
+            Instruction::U64 => parse_varint::<u64>(&const_stack).to_string(),
+            Instruction::U32 => parse_varint::<u32>(&const_stack).to_string(),
+            Instruction::U16 => parse_varint::<u16>(&const_stack).to_string(),
+            Instruction::U8 => parse_varint::<u8>(&const_stack).to_string(),
+            Instruction::I64 => parse_varint::<i64>(&const_stack).to_string(),
+            Instruction::I32 => parse_varint::<i32>(&const_stack).to_string(),
+            Instruction::I16 => parse_varint::<i16>(&const_stack).to_string(),
+            Instruction::I8 => parse_varint::<i8>(&const_stack).to_string(),
             Instruction::F32 => todo!(),
             Instruction::F64 => todo!(),
             Instruction::VectorF32 => todo!(),
             Instruction::VectorF64 => todo!(),
         };
+
+        if x == Instruction::U64
+            || x == Instruction::U32
+            || x == Instruction::U16
+            || x == Instruction::U8
+            || x == Instruction::I64
+            || x == Instruction::I32
+            || x == Instruction::I16
+            || x == Instruction::I8
+        {
+            const_stack.clear()
+        }
 
         stack.push(string);
     }
